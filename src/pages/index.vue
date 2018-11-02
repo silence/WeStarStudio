4067f47ee96670f167f232c072784fed581e10aa
silence <silence.since2017@gmail.com>
2018-11-02 23:44:19 +0800

    feat: add login page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,22 @@
 <template>
-  <div class="container" @click="clickHandle('test click', $event)">
+    <div>
+        <a href="/pages/login">登陆页</a>
+    </div>
+  <!-- <div class="container" @click="clickHandle('test click', $event)">
 
     <div class="userinfo" @click="bindViewTap">
       <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
       <div class="userinfo-nickname">
         <card :text="userInfo.nickName"></card>
       </div>
+    </div>
+
+    <div>
+      <button open-type="getUserInfo" lang="zh_CN" @getuserinfo="test">获取用户信息</button>
+    </div>
+
+    <div>
+      <button open-type="getPhoneNumber" @getPhoneNumber="phoneTest">获取手机号</button>
     </div>
 
     <div class="usermotto">
@@ -19,88 +30,100 @@
       <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
     </form>
     <a href="/pages/counter" class="counter">去往Vuex示例页面</a>
-  </div>
+  </div> -->
 </template>
 
 <script>
 import card from '@/components/card'
 
 export default {
-  data () {
-    return {
-      motto: 'Hello World',
-      userInfo: {}
-    }
-  },
-
-  components: {
-    card
-  },
-
-  methods: {
-    bindViewTap () {
-      const url = '/packageA/logs'
-      wx.navigateTo({ url })
-    },
-    getUserInfo () {
-      // 调用登录接口
-      wx.login({
-        success: () => {
-          wx.getUserInfo({
-            success: (res) => {
-              this.userInfo = res.userInfo
-            }
-          })
+    data() {
+        return {
+            motto: 'Hello World',
+            userInfo: {}
         }
-      })
     },
-    clickHandle (msg, ev) {
-      // eslint-disable-next-line
-      console.log('clickHandle:', msg, ev)
-    }
-  },
 
-  created () {
-    // 调用应用实例的方法获取全局数据
-    this.getUserInfo()
-  }
+    mounted() {
+        wx.redirectTo('/pages/login')
+    },
+
+    components: {
+        card
+    },
+
+    methods: {
+        bindViewTap() {
+            const url = '/packageA/logs'
+            wx.navigateTo({ url })
+        },
+        getUserInfo() {
+            // 调用登录接口
+            console.log('getUserinfo...........')
+            wx.login({
+                success: e => {
+                    console.log(e)
+                    // wx.getUserInfo({
+                    //     success: res => {
+                    //         this.userInfo = res.userInfo
+                    //     }
+                    // })
+                }
+            })
+        },
+        clickHandle(msg, ev) {
+            // eslint-disable-next-line
+            //console.log('clickHandle:', msg, ev)
+        },
+        test(e) {
+            console.log(e)
+        },
+        phoneTest(e) {
+            console.log(e)
+        }
+    }
+
+    // created() {
+    //     // 调用应用实例的方法获取全局数据
+    //     this.getUserInfo()
+    // }
 }
 </script>
 
 <style scoped>
-.userinfo {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+/* .userinfo {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 }
 
 .userinfo-avatar {
-  width: 128rpx;
-  height: 128rpx;
-  margin: 20rpx;
-  border-radius: 50%;
+    width: 128rpx;
+    height: 128rpx;
+    margin: 20rpx;
+    border-radius: 50%;
 }
 
 .userinfo-nickname {
-  color: #aaa;
+    color: #aaa;
 }
 
 .usermotto {
-  margin-top: 150px;
+    margin-top: 150px;
 }
 
 .form-control {
-  display: block;
-  padding: 0 12px;
-  margin-bottom: 5px;
-  border: 1px solid #ccc;
+    display: block;
+    padding: 0 12px;
+    margin-bottom: 5px;
+    border: 1px solid #ccc;
 }
 
 .counter {
-  display: inline-block;
-  margin: 10px auto;
-  padding: 5px 10px;
-  color: blue;
-  border: 1px solid blue;
-}
+    display: inline-block;
+    margin: 10px auto;
+    padding: 5px 10px;
+    color: blue;
+    border: 1px solid blue;
+} */
 </style>
