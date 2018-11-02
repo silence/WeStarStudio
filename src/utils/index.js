2abc09533ea9a05902d26b29b89a52a622a98cca
silence <silence.since2017@gmail.com>
2018-11-02 23:44:46 +0800

    feat: add throttle function

--- src/utils/index.js
+++ src/utils/index.js
@@ -18,6 +18,17 @@ export function formatTime (date) {
   return `${t1} ${t2}`
 }
 
+export function throttle (func, delay) {
+  var last = 0
+  return function (...args) {
+    var curr = +new Date()
+    if (curr - last > delay) {
+      func.apply(this, args)
+      last = curr
+    }
+  }
+}
+
 export default {
   formatNumber,
   formatTime
