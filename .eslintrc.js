b905b0121192485ff9435dce1f96f1b22f13c41d
silence <silence.since2017@gmail.com>
2018-11-02 23:43:41 +0800

    feat: remove some eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,17 +14,17 @@ module.exports = {
   // https://github.com/standard/standard/blob/master/docs/RULES-en.md
   extends: 'standard',
   // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
+  plugins: ['html'],
   // add your custom rules here
-  'rules': {
+  rules: {
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    indent: 'off',
+    'space-before-function-paren': 'off'
   },
   globals: {
     App: true,
